594f7930d2565a36a90ab9000e889d47330d98df
Jon Burgos <jonburgos4@gmail.com>
2023-03-03 18:14:47 +0100

    creacion de interfaz y servicio

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/interfaces/user.interface';
+import { UsersService } from 'src/app/services/users.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit {
+  arrUser: User[] = [];
+
+  constructor(private usersService: UsersService) {}
+
+  async ngOnInit(): Promise<void> {
+    try {
+      let response = await this.usersService.getAll();
+      this.arrUser = response.results;
+      console.log(this.arrUser);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
